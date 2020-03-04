2bacfbc7ee55629e80e28dcb12b681544059c801
Alina Hulkovska <51539228+khamraeva@users.noreply.github.com>
2020-03-04 15:30:32 +0200

    Updates

---
+++ precache-manifest.18a4b4775df74d2602b2ec95a4f360df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae56387c6bbda2c1fef2dfffc21ca569",
+    "url": "/kasta/index.html"
+  },
+  {
+    "revision": "bf266b7a30a8fd335083",
+    "url": "/kasta/static/css/main.145e98d3.chunk.css"
+  },
+  {
+    "revision": "b80efc8b967041907508",
+    "url": "/kasta/static/js/2.21d8fce6.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/kasta/static/js/2.21d8fce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf266b7a30a8fd335083",
+    "url": "/kasta/static/js/main.d8fa1e57.chunk.js"
+  },
+  {
+    "revision": "1d747ae6fde576465327",
+    "url": "/kasta/static/js/runtime-main.19d3664a.js"
+  },
+  {
+    "revision": "c292da4bfca189f791bfa556d3b45756",
+    "url": "/kasta/static/media/kastalogo.c292da4b.png"
+  },
+  {
+    "revision": "5f638d71102db46ba4ce4f2316d5af4f",
+    "url": "/kasta/static/media/logo.5f638d71.png"
+  },
+  {
+    "revision": "5f267694bff613838752856c1bfe50a2",
+    "url": "/kasta/static/media/mastercard.5f267694.png"
+  },
+  {
+    "revision": "3141adf733e47ddd5089f568c47b2403",
+    "url": "/kasta/static/media/visa.3141adf7.png"
+  }
+]);
